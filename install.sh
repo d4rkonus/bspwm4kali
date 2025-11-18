306aee6deda753308eacfaa084630f776c9d2614
d4rkonus <d4rkonus@gmail.com>
2025-11-18 21:58:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,3 @@ sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev li
                  libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev \
                  libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
 
-cd /home/s4vitar/Descargas/
-git clone https://github.com/baskerville/bspwm.git
-git clone https://github.com/baskerville/sxhkd.git
-cd bspwm/
-make
-sudo make install
-cd ../sxhkd/
-make
-sudo make install
