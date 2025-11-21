0e707f9b6f9c5c99aadef6c0cacf9404e393179a
d4rkonus <d4rkonus@gmail.com>
2025-11-21 10:42:11 +0000

    0

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ fi
 echo -e "${yellowColour}[*]${endColour} Installing required packages...\n"
 sudo apt install -y build-essential rofi git vim xcb libxcb-util0-dev libxcb-xinerama0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev &> /dev/null
 
-mkdir ~/github
+mkdir -p ~/github
 
 # Clonar repositorios
 echo -e "${yellowColour}[*]${endColour} Cloning required repositories...\n"
@@ -47,12 +47,12 @@ sudo make install &> /dev/null
 # Cargar repositorios
 echo -e "${yellowColour}[*]${endColour} Creating configuration files...\n"
 sudo apt install -y bspwm &> /dev/null
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
 cd ~/github/bspwm/
-cp examples/bspwmrc ~/.config/bspwm/
+cp "$ruta/config/bspwmrc" ~/.config/bspwm/
 chmod +x ~/.config/bspwm/bspwmrc 
-cp ~/"$ruta/config/sxhkdrc" ~/.config/sxhkd/
+cp "$ruta/config/sxhkdrc" ~/.config/sxhkd/
 
 
 echo -e "The end of the installation script has been reached.\n"
