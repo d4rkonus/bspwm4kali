746aacb252c0b26fb78f85442214891972da5e74
d4rkonus <d4rkonus@gmail.com>
2025-11-21 11:20:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,5 +54,28 @@ cp "$ruta/config/bspwmrc" ~/.config/bspwm/
 chmod +x ~/.config/bspwm/bspwmrc 
 cp "$ruta/config/sxhkdrc" ~/.config/sxhkd/
 
+# Instalar polybar
+echo -e "${yellowColour}[*]${endColour} Installing polybar...\n"
+sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev &> /dev/null
+
+cd /home/$USER/Downloads/
+git clone --recursive https://github.com/polybar/polybar
+cd polybar/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+
+# Configurar polybar
+echo -e "${yellowColour}[*]${endColour} Copying configuration files for polybar...\n"
+cd /home/$USER/Downloads/
+git clone https://github.com/ibhagwan/picom.git
+cd picom/
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
 
 echo -e "The end of the installation script has been reached.\n"
