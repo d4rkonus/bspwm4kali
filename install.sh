84cc0888c624ff3df04b6532612beaa01798baec
d4rkonus <d4rkonus@gmail.com>
2025-11-22 18:37:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,44 @@
 tput civis
 trap 'tput cnorm; exit' INT TERM EXIT
 
-install_depencies(){
-    sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev make cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev feh scrot scrub rofi xclip bat locate ranger neofetch wmname acpi bspwm sxhkd imagemagick >/dev/null
+install_dependencies() {
+    echo "Instalando dependencias..."
+    sudo apt install -y \
+        build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
+        libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev \
+        libxcb-xtest0-dev libxcb-shape0-dev make cmake-data pkg-config python3-sphinx \
+        libcairo2-dev libxcb1-dev libxcb-composite0-dev python3-xcbgen xcb-proto \
+        libxcb-image0-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libpulse-dev \
+        libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev meson libxext-dev \
+        libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev \
+        libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 \
+        libpcre3-dev feh scrot scrub rofi xclip bat locate ranger wmname acpi bspwm \
+        sxhkd imagemagick > /dev/null
 }
 
+import_repositories(){
+    mkdir -p ~/testing
+    cd ~/testing
+    git clone --recursive https://github.com/polybar/polybar > /dev/null
+    git clone https://github.com/ibhagwan/picom.git >/dev/null
+}
+
+install_polybar(){
+    cd ~/testing/polybar 
+    mkdir build
+    cd build 
+    cmake .. >/dev/null
+    sudo make install >/dev/null
+}
+
+finally(){
+    echo "Script finished."
+}
+
+
 install_depencies
+import_repositories
+install_polybar
+finally
 
 tput cnorm
