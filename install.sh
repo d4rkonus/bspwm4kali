557f227ad013b52de67bb33ed1dc47e56acccbd4
d4rkonus <d4rkonus@gmail.com>
2025-11-22 19:07:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ install_dependencies() {
     sudo apt-get install -y \
         build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
         libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev \
-        libxcb-xtest0-dev libxcb-shape0-dev make cmake-data pkg-config python3-sphinx \
+        libxcb-xtest0-dev libxcb-shape0-dev make cmake cmake-data pkg-config python3-sphinx \
         libcairo2-dev libxcb1-dev libxcb-composite0-dev python3-xcbgen xcb-proto \
         libxcb-image0-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libpulse-dev \
         libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev meson libxext-dev \
