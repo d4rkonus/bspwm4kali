144fb305f113b86868dc935ff94ca8355b42806b
d4rkonus <d4rkonus@gmail.com>
2025-11-22 10:24:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ echo -e "${yellowColour}[*]${endColour} Installing polybar...\n"
 sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev &> /dev/null
 
 cd /home/$USER/Downloads/
-git clone --recursive https://github.com/polybar/polybar
+git clone --recursive https://github.com/polybar/polybar &> /dev/null
 cd polybar/
 mkdir build
 cd build/
