0e1c4d4a0e9107715c5993ed530e94f6c01d3207
d4rkonus <d4rkonus@gmail.com>
2025-11-22 13:05:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ mkdir -p ~/.config/polybar
 cd blue-sky/polybar/   
 cp * -r ~/.config/polybar &> /dev/null
 echo "~/.config/polybar/launch.sh &" >> ~/.config/bspwm/bspwmrc 2>/dev/null
-echo "bspc config focus_follows_pointer true" >> ~/.config/bspwm/bspwmrc &> /dev/null
+echo "bspc config focus_follows_pointer true" >> ~/.config/bspwm/bspwmrc 2>/dev/null
 cd fonts/
 sudo cp * /usr/share/fonts/truetype/ &> /dev/null
 fc-cache -v &> /dev/null
@@ -106,8 +106,8 @@ echo -e "${yellowColour}[*]${endColour} Applying customizations to polybar...\n"
 mkdir ~/.config/picom
 cd ~/.config/picom
 cp -r "$ruta/config/picom.conf" ~/.config/picom/ &> /dev/null
-echo "picom &" >> ~/.config/bspwm/bspwmrc 
-echo "bspc config border_width 0 &" >> ~/.config/bspwm/bspwmrc
+echo "picom &" >> ~/.config/bspwm/bspwmrc 2>/dev/null
+echo "bspc config border_width 0 &" >> ~/.config/bspwm/bspwmrc 2>/dev/null
 
 # Move fonts
 echo -e "${yellowColour}[*]${endColour} Moving font files...\n"
